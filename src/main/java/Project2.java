a523eb61f0a381fb1685a9bd4031cb9604f4420a
mcballard <mattb315@yahoo.com>
2022-04-14 10:45:40 -0400

    added connection object for db

--- src/main/java/Project2.java
+++
@@ -1,5 +0,0 @@
-public class Project2 {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
---
+++ src/main/java/com/businessName/ticketDao/ConnectionObject.java
@@ -0,0 +1,29 @@
+package com.businessName.ticketDao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionObject {
+    public static Connection createConnection() {
+        try {
+            Connection connection =
+                    DriverManager.getConnection(String.format(
+                            "jdbc:postgresql://%s:%s/%s?user=%s&password=%s",
+                            System.getenv("HOST"),
+                            System.getenv("PORT"),
+                            System.getenv("DBNAME"),
+                            System.getenv("DBUSER"),
+                            System.getenv("DBPASS")
+                    ));
+            return connection;
+        } catch(SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(createConnection());
+    }
+}
