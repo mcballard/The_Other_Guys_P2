e6237f1bcc50ed91016acaf489a0d894ac97a87d
jraghuraj <jraghuraj@ufl.edu>
2022-04-19 16:23:03 -0400

    updatehelp

--- src/main/java/com/businessName/ticketService/ClientInteractions.java
+++ src/main/java/com/businessName/ticketService/ClientInteractions.java
@@ -1,10 +1,13 @@
 package com.businessName.ticketService;
 
+import com.businessName.MalformedObjectException.MalformedObjectException;
 import com.businessName.dataEntity.DatabaseEntity;
 import com.businessName.ticketDao.DataAccessImp;
 import com.businessName.ticketDao.DataAccessInterface;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -19,7 +22,21 @@ public class ClientInteractions extends EmployeeInteractions{
 
     public String viewHelpRequest(String jsonFromApi) { return null; }
 
-    public String updateHelpRequest(String jsonFromApi) { return null; }
+    public String updateHelpRequest(String jsonFromApi) {
+        HashMap<String, String> updateMap = new Gson().fromJson(
+                String.valueOf(jsonFromApi),
+                new TypeToken<HashMap<String, String>>() {}.getType());
+        DatabaseEntity updateRequest = new DatabaseEntity(updateMap);
+        updateRequest.sanitizeFromApi();
+        if(updateRequest.newRowObject.containsKey("employee_id")) {
+            HashMap<String, String> databaseResponse = daoObject.updateObjectDb(updateRequest.returnSqlForUpdateOne()).newRowObject;
+            JSONObject updateRequestJson = new JSONObject(databaseResponse);
+            return String.valueOf(updateRequestJson);
+        }
+        else {
+            throw new RecordNotFound("no active request");
+        }
+    }
 
     public String cancelHelpRequest(String jsonFromApi) { return null; }
 }
