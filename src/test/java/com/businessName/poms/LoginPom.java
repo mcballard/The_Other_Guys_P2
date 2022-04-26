a65ea3939a2ca262707c83d3784b0d4772305fd8
mcballard <mattb315@yahoo.com>
2022-04-26 17:06:13 -0400

    cleaned up e2e tests

--- src/test/java/com/businessName/poms/LoginPom.java
+++ src/test/java/com/businessName/poms/LoginPom.java
@@ -26,8 +26,29 @@ public class LoginPom {
     @FindBy(xpath="/html/body/div/div/div[3]/button[1]")
     public WebElement sweetalertContinueButton;
 
+    @FindBy(xpath = "/html/body/div[6]/div/div[3]/button[1]")
+    public WebElement SweetalertClientCreateContinueButton;
+
+    @FindBy(xpath = "/html/body/button[2]")
+    public  WebElement updatePersonalInfoCollapsible;
+
+    @FindBy(id="updateInfo")
+    public  WebElement updatePersonalInfo;
+
+    @FindBy(id="firstName")
+    public WebElement firstNameInput;
+
+    @FindBy(id="lastName")
+    public WebElement lastNameInput;
+
+    @FindBy(id="help_request_comment")
+    public WebElement createHelpRequestCommentBox;
+
     @FindBy(id="createCollapseButton")
     public WebElement createRequestCollapsibleClient;
+
+    @FindBy(id="create_request")
+    public WebElement createRequestSubmitButton;
 
     @FindBy(id="cancelCollapseButton")
     public WebElement cancelCollapseButton;
@@ -38,17 +59,32 @@ public class LoginPom {
     @FindBy(id="viewCollapseButton")
     public WebElement viewCollapseButton;
 
+    @FindBy(id="view_request")
+    public WebElement viewRequestSubmitButton;
+
     @FindBy(id="help_request_comment")
     public WebElement helpRequestComment;
 
     @FindBy(id="update_ticket_comment")
     public WebElement updateTicketComment;
 
-     @FindBy(xpath="/html/body/button[1]")
-    public WebElement updatePersonalInfo;
-
-     @FindBy(id="employeeUpdate")
+    @FindBy(id="employeeUpdate")
     public WebElement employeeUpdate;
+
+    @FindBy(id="cancel_request")
+    public WebElement cancelRequestSubmitButton;
+
+    @FindBy(id="update_request_comment")
+    public WebElement updateRequestCommentInput;
+
+    @FindBy(id="update_request")
+    public WebElement updateRequestSubmitButton;
+
+    @FindBy(id="update_request_id")
+    public WebElement updateRequestIdInput;
+
+    @FindBy(id="ticket_requests_id")
+    public  WebElement cancelRequestIdInput;
 
 
 
